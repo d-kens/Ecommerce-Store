6095f88884d06e99dc5d8d0a1a449346faa81136
d-kens <omondikens1@gmail.com>
2022-02-17 23:21:00 +0300

    Created Main Menu

--- index.php
+++ index.php
@@ -47,7 +47,102 @@
                 </div><!--col-md-6 end-->
 
             </div><!--Container End-->
-        </div><!--Top Begin-->
+        </div><!--Top end-->
+
+        <div id="navbar" class="navbar navbar-default"><!-- navbar navbar-default begin-->
+
+            <div class="container"><!--Container begin-->
+
+                <div class="navbar-header"><!--Navbar-header begin-->
+
+                    <a href="index.php" class="navbar-brand home"><!--Navbar-brand home begin-->
+
+                        <img src="images/ecom-store-logo.png" alt="Store-Logo" class="hidden-xs">
+                        <img src="images/ecom-store-logo-mobile.png" alt="Store-Logo" class="visible-xs">
+
+                    </a><!--Navbar-brand home ends-->
+                    <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <i class="fa fa-align-justify"></i>
+                    </button>
+                    <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                        <span class="sr-only">Toggle Search</span>
+                        <i class="fa fa-search"></i>
+                    </button>
+
+                </div><!--Nabar-header end-->
+
+                <div class="navbar-collapse collapse" id="navigation"><!--navbar-collapse collapse begin-->
+
+                    <div class="padding-nav"><!--padding-nav begin-->
+
+                        <ul class="nav navbar-nav left"><!--nav navbar-nav left begin-->
+
+                            <li class="active">
+                                <a href="index.php">Home</a>
+                            </li>
+                            <li>
+                                <a href="shop.php">Shop</a>
+                            </li>
+                            <li>
+                                <a href="checkout.php">My Account</a>
+                            </li>
+                            <li>
+                                <a href="cart.php">Shopping Cart</a>
+                            </li>
+                            <li>
+                                <a href="contact.php">Contact Us</a>
+                            </li>
+
+                        </ul><!--nav navbar-nav left end-->
+
+                    </div><!--padding-nav end-->
+
+                    <a href="cart.php" class="btn navbar-btn btn-primary right"><!--btn navbar-btn btn-primary right begin-->
+
+                        <i class="fa fa-shopping-cart"></i>
+                        <span>4 items in your cart</span>
+
+                    </a><!--btn navbar-btn btn-primary right end-->
+
+                    <div class="navbar-collapse collapse right"><!--navbar-collapse collapse right begin-->
+
+                        <button class="btn btn-primary navbar-btn" type="button" data-toggle="collapse" data-target="#search"><!--btn btn-primary navbar-btn begin-->
+
+                            <span class="sr-only">Toggle Search</span>
+                            <i class="fa fa-search"></i>
+
+                        </button><!--btn btn-primary navbar-btn end-->
+
+                    </div><!--navbar-collapse collapse right end-->
+
+                    <div class="collapse clearfix" id="search"><!--collapse clearfix begin-->
+
+                        <form action="result.php" method="get" class="navbar-form"><!--navbar-form begin-->
+
+                            <div class="input-group"><!--input-group begin-->
+
+                                <input type="text" class="form-control" name="user_querry" placeholder="search" required>
+
+                                <span class="input-group-btn"><!--input-group-btn begin-->
+                                    <button type="submit" name="search" value="search" class="btn btn-primary"><!--btn btn-primary begin-->
+
+                                        <i class="fa fa-search"></i>
+
+                                    </button><!--btn btn-primary end-->
+                                </span><!--input-group-btn end-->
+
+                            </div><!--input-group end-->
+
+                        </form><!--navbar-form end-->
+
+                    </div><!--collapse clearfix end-->
+
+                </div><!--navbar-collapse collapse end-->
+
+            </div><!--Container end-->
+
+        </div><!-- navbar navbar-default end-->
 
 
 
