e64739bf9caf8a1646001af82797a8cee6edb14f
d-kens <omondikens1@gmail.com>
2022-02-28 13:08:46 +0300

    Created Shop.php

---
+++ includes/sidebar.php
@@ -0,0 +1,40 @@
+
+<div class="panel panel-default sidebar-menu"><!--"panel panel-default sidebar-menu begin-->
+    <div class="panel-heading"><!--panel-heading begin-->
+        <h3 class="panel-title">Products Categories</h3>
+    </div><!--panel-heading end-->
+
+    <div class="panel-body"><!--panel-body begin-->
+        <ul class="nav nav-pills nav-stacked category-menu"><!--nav nav-pills nav-stacked category-menu begin-->
+
+            <li><a href="">Jackets</a></li>
+            <li><a href="">Accessories</a></li>
+            <li><a href="">Shoes</a></li>
+            <li><a href="">Coats</a></li>
+            <li><a href="">T-Shirts</a></li>
+
+        </ul><!--nav nav-pills nav-stacked category-menu end-->
+    </div><!--panel-body end-->
+
+
+</div><!--"panel panel-default sidebar-menu end-->
+
+
+<div class="panel panel-default sidebar-menu"><!--"panel panel-default sidebar-menu begin-->
+    <div class="panel-heading"><!--panel-heading begin-->
+        <h3 class="panel-title">Categories</h3>
+    </div><!--panel-heading end-->
+
+    <div class="panel-body"><!--panel-body begin-->
+        <ul class="nav nav-pills nav-stacked category-menu"><!--nav nav-pills nav-stacked category-menu begin-->
+
+            <li><a href="">Men</a></li>
+            <li><a href="">Women</a></li>
+            <li><a href="">Kids</a></li>
+            <li><a href="">Pets & Others</a></li>
+
+        </ul><!--nav nav-pills nav-stacked category-menu end-->
+    </div><!--panel-body end-->
+
+
+</div><!--"panel panel-default sidebar-menu end-->
