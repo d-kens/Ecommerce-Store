c704bb47c5ff982c9d2f4984bf59facaacc2bca7
d-kens <omondikens1@gmail.com>
2022-02-28 11:45:27 +0300

    Created Footer

--- index.php
+++ index.php
@@ -539,12 +539,12 @@
 
         </div><!--container end-->
 
+        <!--Footer-->
+        <?php
 
+            include("includes/footer.php");
 
-
-
-
-
+        ?>
 
         <script src="js/jquery-331.min.js"></script>
         <script src="js/bootstrap-337.min.js"></script>
