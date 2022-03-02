8b9cc49ebdf6857745eba4a65b176ff77125d3cd
d-kens <omondikens1@gmail.com>
2022-03-02 17:58:07 +0300

    Create cart.php

--- cart.php
+++ cart.php
@@ -81,13 +81,13 @@
                             <li>
                                 <a href="index.php">Home</a>
                             </li>
-                            <li class="active">
+                            <li>
                                 <a href="shop.php">Shop</a>
                             </li>
                             <li>
                                 <a href="checkout.php">My Account</a>
                             </li>
-                            <li>
+                            <li class="active">
                                 <a href="cart.php">Shopping Cart</a>
                             </li>
                             <li>
@@ -150,161 +150,231 @@
 
                     <ul class="breadcrumb"><!--breadcrumb begin-->
                         <li><a href="index.php">Home</a></li>
-                        <li>Shop</li>
+                        <li>Cart</li>
                     </ul><!--breadcrumb end-->
 
                 </div><!--col-md-12 end-->
-                <div class="col-md-3"><!--col-md-3 begin-->
 
-                    <!--side bar-->
-                    <?php
+                <div id="cart" class="col-md-9"><!--col-md-9 begin-->
 
-                    include("includes/sidebar.php");
+                    <div class="box"><!--box begin-->
 
-                    ?>
+                        <form action="cart.php" method="post" enctype="multipart/form-data"><!--form begin-->
 
-                </div><!--col-md-3 end-->
+                            <h1>Shopping Cart</h1>
+                            <p class="text-muted">You currently have 3 item(s) in your cart</p>
 
-                <div class="col-md-9"><!--col-md-9 begin-->
-                    <div id="productMain" class="row"><!--row begin-->
-                        <div class="col-sm-6"><!--col-sm-6 begin-->
-                            <div id="mainImage"><!--mainImage begin-->
-                                <div id="myCarousel" class="carousel slide"><!--carousel slide begin-->
-                                    <ol class="carousel-indicators"><!--carousel-indicators begin-->
-                                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                                        <li data-target="#myCarousel" data-slide-to="1"></li>
-                                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                                    </ol><!--carousel-indicators end-->
+                            <div class="table-responsive"><!--table-responsive begin-->
 
-                                    <div class="carousel-inner">
-                                        <div class="item active">
-                                            <center><img class="img-responsive" src="admin_area/product_images/product-3.jpg" alt="Product 3"></center>
-                                        </div>
-                                        <div class="item">
-                                            <center><img class="img-responsive" src="admin_area/product_images/product-4.jpg" alt="Product 4"></center>
-                                        </div>
-                                        <div class="item">
-                                            <center><img class="img-responsive" src="admin_area/product_images/product-5.jpg" alt="Product 5"></center>
-                                        </div>
-                                    </div>
+                                <table class="table"><!--table begin-->
 
-                                    <a href="#myCarousel" class="left carousel-control" data-slide="prev"><!--left carousel-control begin-->
-                                        <span class="glyphicon glyphicon-chevron-left"></span>
-                                        <span class="sr-only">Previuous</span><!--left carousel-control end-->
+                                    <thead><!--thead begin-->
+                                        <tr><!--tr begin-->
+
+                                            <th colspan="2">Product</th>
+                                            <th>Quantity</th>
+                                            <th>Unit Price</th>
+                                            <th>Size</th>
+                                            <th colspan="1">Delete</th>
+                                            <th colspan="2">Sub-Total</th>
+
+                                        </tr><!--tr end-->
+                                    </thead><!--thead end-->
+
+                                    <tbody><!--tbody begin-->
+
+                                        <tr><!--tr begin-->
+
+                                            <td>
+
+                                                <img class="img-responsive" src="admin_area/product_images/product-1.jpg" alt="Product 1">
+
+                                            </td>
+
+                                            <td>
+                                                <a href="#">Product 1</a>
+                                            </td>
+
+                                            <td>
+
+                                                2
+
+                                            </td>
+
+                                            <td>
+
+                                                ksh : 3000
+
+                                            </td>
+
+                                            <td>
+
+                                                Large
+
+                                            </td>
+
+                                            <td>
+
+                                                <input type="checkbox" name="remove[]">
+
+                                            </td>
+
+                                            <td>
+
+                                                Ksh: 30000
+
+                                            </td>
+
+                                        </tr><!--tr end-->
+
+                                    </tbody><!--tbody end-->
+
+                                    <tbody><!--tbody begin-->
+
+                                        <tr><!--tr begin-->
+
+                                            <td>
+
+                                                <img class="img-responsive" src="admin_area/product_images/product-1.jpg" alt="Product 1">
+
+                                            </td>
+
+                                            <td>
+                                                <a href="#">Product 1</a>
+                                            </td>
+
+                                            <td>
+
+                                                2
+
+                                            </td>
+
+                                            <td>
+
+                                                ksh : 3000
+
+                                            </td>
+
+                                            <td>
+
+                                                Large
+
+                                            </td>
+
+                                            <td>
+
+                                                <input type="checkbox" name="remove[]">
+
+                                            </td>
+
+                                            <td>
+
+                                                Ksh: 30000
+
+                                            </td>
+
+                                        </tr><!--tr end-->
+
+                                    </tbody><!--tbody end-->
+
+                                    <tbody><!--tbody begin-->
+
+                                        <tr><!--tr begin-->
+
+                                            <td>
+
+                                                <img class="img-responsive" src="admin_area/product_images/product-1.jpg" alt="Product 1">
+
+                                            </td>
+
+                                            <td>
+                                                <a href="#">Product 1</a>
+                                            </td>
+
+                                            <td>
+
+                                                2
+
+                                            </td>
+
+                                            <td>
+
+                                                ksh : 3000
+
+                                            </td>
+
+                                            <td>
+
+                                                Large
+
+                                            </td>
+
+                                            <td>
+
+                                                <input type="checkbox" name="remove[]">
+
+                                            </td>
+
+                                            <td>
+
+                                                Ksh: 30000
+
+                                            </td>
+
+                                        </tr><!--tr end-->
+
+                                    </tbody><!--tbody end-->
+
+                                    <tfoot><!--tfoot begin-->
+
+                                        <tr><!--tr begin-->
+
+                                            <th colspan="5">Total</th>
+                                            <th colspan="2">Ksh: 30000</th>
+
+                                        </tr><!--tr end-->
+
+                                    </tfoot><!--tfoot end-->
+
+                                </table><!--table end-->
+
+                            </div><!--table responsive end-->
+
+                            <div class="box-footer"><!--box-footer begin-->
+
+                                <div class="pull-left"><!--pull-left begin-->
+
+                                    <a href="index.php" class="btn btn-default"><!--btn btn-default begin-->
+
+                                        <i class="fa fa-chevron-left"></i> Continue Shopping
+
+                                    </a><!--btn btn-default end-->
+
+                                </div><!--pull-left end-->
+
+                                <div class="pull-right"><!--pull-right begin-->
+
+                                    <button type="submit" name="update" value="Update Cart" href="index.php" class="btn btn-default"><!--btn btn-default begin-->
+
+                                        <i class="fa fa-refresh"></i> Update Cart
+
+                                    </button><!--btn btn-default end-->
+
+                                    <a href="checkout.php" class="btn btn-primary">
+
+                                        Proceed Checkout <i class="fa fa-chevron-right"></i>
+
                                     </a>
-                                    <a href="#myCarousel" class="right carousel-control" data-slide="next"><!--right carousel-control begin-->
-                                        <span class="glyphicon glyphicon-chevron-right"></span>
-                                        <span class="sr-only">Next</span>
-                                    </a><!--right carousel-control end-->
 
-                                </div><!--carousel slide end-->
-                            </div><!--mainImage end-->
-                        </div><!--col-sm-6 end-->
+                                </div><!--pull-right end-->
 
-                        <div class="col-sm-6"><!--col-sm-6 begin-->
-                            <div class="box"><!--box begin-->
-                                <h1 class="text-center">Jumper Jumper</h1>
+                            </div><!--box-footer end-->
 
-                                <form action="details.php" class="form-horizontal" method="post"><!--form-horizontal begin-->
-                                    <div class="form-group"><!--form-group begin-->
-                                        <label for="" class="col-md-5 control-label">Product Quantity</label>
-
-                                        <div class="col-md-7"><!--col-md-7 begin-->
-                                            <select name="product_qty" id="" class="form-control"><!--select begin-->
-                                                <option>1</option>
-                                                <option>2</option>
-                                                <option>3</option>
-                                                <option>4</option>
-                                                <option>5</option>
-                                                <option>6</option>
-                                                <option>7</option>
-                                                <option>8</option>
-                                                <option>9</option>
-                                                <option>10</option>
-                                                <option>11</option>
-                                                <option>12</option>
-                                                <option>13</option>
-                                                <option>14</option>
-                                                <option>15</option>
-                                            </select><!--select end-->
-                                        </div><!--col-md-7 end-->
-
-                                    </div><!--form-group end-->
-
-                                    <div class="form-group"><!--form-group begin-->
-                                        <label class="col-md-5 control-label">Product Size</label>
-
-                                        <div class="col-md-7"><!--col-md-7 begin-->
-
-                                            <select name="product_size" class="form-control"><!--select begin-->
-                                                <option>Select a size</option>
-                                                <option>Small</option>
-                                                <option>Medium</option>
-                                                <option>Large</option>
-                                            </select><!--select end-->
-
-                                        </div><!--col-md-7 end-->
-
-                                    </div><!--form-group end-->
-
-                                    <p class="price text-center">Ksh: 3000</p>
-
-                                    <p class="text-center buttons">
-                                        <button class="btn btn-primary i fa fa-shopping-cart"> Add to Cart</button>
-                                    </p>
-
-                                </form><!--form-horizontal end-->
-
-
-                            </div><!--box end-->
-
-                            <div class="row" id="thumbs"><!--row begin-->
-
-                                <div class="col-xs-4"><!--col-xs-4 begin-->
-                                    <a data-target="#myCarousel" data-slide-to="0" href="" class="thumb"><!--thumb begin-->
-                                        <img src="admin_area/product_images/product-3.jpg" alt="Product 3" class="img-responsive">
-                                    </a><!--thumb end-->
-                                </div><!--col-xs-4 end-->
-
-                                <div class="col-xs-4"><!--col-xs-4 begin-->
-                                    <a data-target="#myCarousel" data-slide-to="1" href="" class="thumb"><!--thumb begin-->
-                                        <img src="admin_area/product_images/product-4.jpg" alt="Product 3" class="img-responsive">
-                                    </a><!--thumb end-->
-                                </div><!--col-xs-4 end-->
-
-                                <div class="col-xs-4"><!--col-xs-4 begin-->
-                                    <a data-target="#myCarousel" data-slide-to="2" href="" class="thumb"><!--thumb begin-->
-                                        <img src="admin_area/product_images/product-5.jpg" alt="Product 4" class="img-responsive">
-                                    </a><!--thumb end-->
-                                </div><!--col-xs-4 end-->
-
-                            </div><!--row finish-->
-
-
-                        </div><!--col-sm-6 end-->
-
-                    </div><!--row end-->
-
-                    <div class="box" id="details"><!--box begin-->
-
-                       <h4>Product Details</h4>
-
-                       <p>
-                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium distinctio, vel laborum cum maxime quas ipsum.
-                       </p>
-
-                       <h4>Size</h4>
-
-                       <ul>
-                           <li>Small</li>
-                           <li>Medium</li>
-                           <li>Large</li>
-                       </ul>
-
-                       <hr>
+                        </form><!--form end-->
 
                     </div><!--box end-->
 
+                    
                     <div id="row same-height-row"><!--row same-height-row begin-->
                         <div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 begin-->
                             <div class="box same-height headline"><!--box same-height headline begin-->
@@ -360,8 +430,59 @@
 
                     </div><!--row same-height-row end-->
 
-                </div><!--col-md-9 end-->
+                </div><!--col -md-9 end-->
 
+                <div class="col-md-3"><!--col-md-3 begin-->
+
+                    <div id="order-summary" class="box"><!--box start-->
+
+                        <div class="box-header"><!--box-header begin-->
+
+                            <h3>Order Summary</h3>
+
+                        </div><!--box-header end-->
+
+                        <p class="text-muted"><!--text-muted begin-->
+
+                            Shipping and additional costs are calculated based on value you have entered
+
+                        </p><!--text-muted end-->
+
+                        <div class="table-responsive"><!--table-responsive begin-->
+
+                            <table class="table"><!--table begin-->
+
+                                <tbody><!--tbody begin-->
+
+                                    <tr><!--tr begin-->
+                                        <td>Order Sub-Total</td>
+                                        <td>ksh:30000</td>
+                                    </tr><!--tr end-->
+
+                                    <tr><!--tr begin-->
+                                        <td>Shipping and Handling</td>
+                                        <td>ksh:0</td>
+                                    </tr><!--tr end-->
+
+                                    <tr><!--tr begin-->
+                                        <td>Tax</td>
+                                        <td>ksh:0</td>
+                                    </tr><!--tr end-->
+
+                                    <tr class="total"><!--tr begin-->
+                                        <td>Total</td>
+                                        <th>ksh:30000</th>
+                                    </tr><!--tr end-->
+
+                                </tbody><!--tbody end-->
+                            
+                            </table><!--table end-->
+
+                        </div><!--table-responsive end-->
+
+                    </div><!--box end-->
+
+                </div><!--col-md-3 end-->
 
             </div><!--container end-->
         </div><!--content end-->
